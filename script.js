458a54ef4781194dcd4329f18b258ab201adb0a8
mxxnl1ghtx <158047032+mxxnl1ghtx@users.noreply.github.com>
2024-02-16 00:25:49 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,32 +5,74 @@ tg.expand()
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37"
 
-let item = "";
+let items = [];
 
-let buttons = document.querySelectorAll(".btn");
-buttons.forEach(el => {
-    el.addEventListener('click', event =>{
-        let price = el.getAttribute('.price');
-        if (tg.MainButton.isVisible) {
-            tg.MainButton.hide();
+function toggleItem(btn, itemId, price) {
+    let item = items.find(i => i.id === itemId);
+    if (!item) {
+        let newItem = {id: itemId, price: price}
+        items.push(newItem);
+        btn.classList.add('added-to-cart');
+        btn.innerText = "Удалить";
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
         } else {
-            tg.MainButton.setText("Вы выбрали товар " + price);
-            item = "el";
-            tg.MainButton.show();
+            tg.MainButton.hide();
         }
-    }); 
-}); 
+    } else {
+        let index = items.indexOf(item);
+        items.splice(index, 1);
+        btn.classList.remove('added-to-cart');
+        btn.innerText = 'Добавить';
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
+            if (!tg.MainButton.isVisible) {
+                tg.MainButton.show();
+            }
+        } else {
+            tg.MainButton.hide();
+        }
+    }
+}
+
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(item);
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    };
+    tg.sendData(JSON.stringify(data));
 })
 
-let usercard = document.getElementById("usercard");
+function calculateTotalPrice() {
+    return items.reduce((total, item) => total + item.price, 0);
+}
 
-let p = document.createElement("p");
+document.getElementById("btn1").addEventListener("click", function() {
+    toggleItem(this, "item1", 2100)
+})
 
-usercard.appendChild(p)
+document.getElementById("btn2").addEventListener("click", function() {
+    toggleItem(this, "item2", 2500)
+})
 
-p.innerHTML = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+document.getElementById("btn3").addEventListener("click", function() {
+    toggleItem(this, "item3", 2300)
+})
 
+document.getElementById("btn4").addEventListener("click", function() {
+    toggleItem(this, "item4", 2700)
+})
+
+document.getElementById("btn5").addEventListener("click", function() {
+    toggleItem(this, "item5", 2900)
+})
+
+document.getElementById("btn6").addEventListener("click", function() {
+    toggleItem(this, "item6", 3200)
+})
